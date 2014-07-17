4761ad4c1e2a314a98938305920668d2130b7432
Vy-Shane Sin Fat <shane@node.mu>
2014-07-17 22:46:54 +0800

    Don't rely on the ordering of the fields when parsing location data. Added user agent string to web service call.

--- lib/weather/location.ex
+++ lib/weather/location.ex
@@ -3,14 +3,16 @@ defmodule Weather.Location do
   @moduledoc "Handle fetching of location data from the Google Maps service."
 
   alias HTTPotion.Response
+  alias Weather.Location
 
-  @user_agent ["User-agent": "Elixir Weather"]
+  @user_agent Application.get_env :weather, :user_agent
   @location_api_url Application.get_env :weather, :google_maps_web_service_url
 
   defstruct name: "", latitude: nil, longitude: nil
 
   @doc """
   location_name is the string to be used to query the web service.
+
   Returns {:ok, %Weather.Location} or {:error, error_message}
   """
   def fetch(location_name) do
@@ -20,8 +22,8 @@ defmodule Weather.Location do
 
   defp _fetch_from_web_service(location_name) do
     response = _location_url(location_name)
-                |> URI.encode
-                |> HTTPotion.get
+      |> URI.encode
+      |> HTTPotion.get @user_agent
 
     return_code = if Response.success?(response), do: :ok, else: :error
     {return_code, response.body}
@@ -42,21 +44,28 @@ defmodule Weather.Location do
   end
 
   defp _parse_location_body([{"results", results}, {"status", "OK"}]) do
+    discard_key = fn {_, list} -> list end
     [best_match | _] = results
 
-    [_address_components,
-      {"formatted_address", location_name},
-      {"geometry", geometry},
-      _types
-    ] = best_match
+    location_name = best_match
+      |> List.keyfind("formatted_address", 0, nil)
+      |> discard_key.()
 
-    [_bounds,
-      {"location", [{"lat", latitude}, {"lng", longitude}]},
-      _location_type,
-      _viewport
-    ] = geometry
+    geometry_location = best_match
+      |> List.keyfind("geometry", 0, nil)
+      |> discard_key.()
+      |> List.keyfind("location", 0, nil)
+      |> discard_key.()
 
-    location = %Weather.Location{
+    latitude = geometry_location
+      |> List.keyfind("lat", 0, nil)
+      |> discard_key.()
+
+    longitude = geometry_location
+      |> List.keyfind("lng", 0, nil)
+      |> discard_key.()
+
+    location = %Location{
       name: location_name,
       latitude: latitude,
       longitude: longitude
